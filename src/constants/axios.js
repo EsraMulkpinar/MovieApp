90c3e8a36c4850d48ceaaf0b3fd3600c4f60e2cd
Esra Nur Mülkpınar <esramulkpinarrr@gmail.com>
2023-11-08 23:24:38 +0300

    .env file added

--- src/constants/axios.js
+++ src/constants/axios.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export const instance = axios.create({
     baseURL: "https://api.themoviedb.org/3",
     params: {
-        api_key:"e079ff309f7a06f9988a5a5685d299a2"
+        api_key:""
     }
 })
